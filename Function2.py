7eecca1543aa4830584a8ca5c52bae67cc81bc50
ssm1016 <min101016@gmail.com>
2021-12-14 10:49:59 +0900

    함수를연습2

--- Function2.py
+++ Function2.py
@@ -62,3 +62,20 @@ def union(*ar):
 print (union ("ham", "spam"))
 print (union ("ham", "spam", "egg"))
 
+
+
+# 정의되지 않은 인자 처리
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL
+
+print (userURIBuilder("credu.com", "80", id="kim", name="mike"))
+print (userURIBuilder("credu.com", "80", id="kim", name="mike", age="30"))
+
+# 람다함수
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+

3f117e4048d9dbeea850fa38b9469d2e2199398a
ssm1016 <min101016@gmail.com>
2021-12-14 11:49:48 +0900

    함수연습

--- Function3.py
+++ Function3.py
@@ -5,3 +5,16 @@ print(result)
 # 년도를 생성
 years = list(range(2000,2022))
 print(years)
+
+# -1 감소
+numbers = list(range(10,0,-1))
+print(numbers)
+
+# 리스트 컴프리헨션(한줄로 반복, 필터링, 가공하기 )*********중요 ********
+# 약식 3개가 붙어있는 코드
+lst = list(range(1,11))
+result = [i**2 for i in lst if i > 5]
+print(result)
+
+# 필터링하는 함수
+
